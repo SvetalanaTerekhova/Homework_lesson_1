dab9826efc7d728e258a775172f0d47adff6a2fc
Svetlana <svetik.terekhova@mail.ru>
2022-08-21 16:04:50 +0300

    add task_8

---
+++ task_8/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число: ");
+double namber = double.Parse(Console.ReadLine());
+double EvenNumber = 1;
+Console.Write("Ответ: ");
+while(EvenNumber<=namber)
+{
+    if (EvenNumber%2 == 0)
+    {
+        Console.Write(EvenNumber);
+        Console.Write(";");        
+    }
+    EvenNumber++; 
+}
